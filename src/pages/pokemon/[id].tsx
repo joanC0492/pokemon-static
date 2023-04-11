45e9c58b7c5ce786e1ee6ec7ad20739794c13ae7
Joan C <joan0492@gmail.com>
2023-04-11 16:43:31 -0500

    --53. Next - getStaticPaths

--- src/pages/pokemon/[id].tsx
+++ src/pages/pokemon/[id].tsx
@@ -1,16 +1,63 @@
-import { Layout } from "@/components/layouts";
-import { NextPage } from "next";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { useRouter } from "next/router";
+import { Layout } from "@/components/layouts";
 
-const PokemonPage: NextPage = () => {
+interface IProps {
+  id: string;
+  name: string;
+}
+const PokemonPage: NextPage<IProps> = ({ id, name }) => {
   const router = useRouter();
-  const { id } = router.query;
   return (
     <Layout title={"Pókemon"}>
-      <div>PokemonPage {id}</div>
+      <div className="container">
+        <div>
+          {id} {name}
+        </div>
+      </div>
     </Layout>
   );
 };
 
-export default PokemonPage;
+// Debe usar getStaticPaths si está pre-renderizando estáticamente páginas que usan rutas dinámicas
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+  // const { data } = await  // your fetch function here
 
+  return {
+    paths: [
+      {
+        params: {
+          id: "1",
+        },
+      },
+      {
+        params: {
+          id: "2",
+        },
+      },
+      {
+        params: {
+          id: "3",
+        },
+      },
+    ],
+    // Debe ir en falso para que los id's que no estan en el arreglo no se consideren y mande al 404
+    fallback: false,
+  };
+};
+
+// Debería usar getStaticProps cuando:
+//- Los datos necesarios para representar la página están disponibles en el momento de la compilación antes de la solicitud del usuario.
+//- Los datos provienen de un CMS sin cabeza.
+//- Los datos se pueden almacenar en caché públicamente (no específicos del usuario).
+//- La página debe estar renderizada previamente (para SEO) y ser muy rápida: getStaticProps genera archivos HTML y JSON, los cuales pueden ser almacenados en caché por un CDN para el rendimiento.
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  return {
+    props: {
+      id: 1,
+      name: "Toma mi pikaChú",
+    },
+  };
+};
+
+export default PokemonPage;
